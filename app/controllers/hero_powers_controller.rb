5effd401bc74d1489e973d4da2bf6e43c8a9f76d
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2023-03-27 12:13:06 +0300

    a get request for all heros

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,3 +1,4 @@
 class HeroPowersController < ApplicationController
-    # get heros
+    
+    
 end
--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,2 +1,7 @@
 class HerosController < ApplicationController
+    # get all heros
+    def index
+        heros= Hero.all
+        render json: heros
+    end
 end
