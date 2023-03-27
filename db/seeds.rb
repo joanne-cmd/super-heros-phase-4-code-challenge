07a7a2710c1377330a86297af0d1961f960caec2
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2023-03-27 11:13:06 +0300

    created a seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+puts "🌱 Seeding ."
+20.times do
+    Hero.create!(
+        name:Faker::Superhero.name,
+        super_name:Faker::Games::Pokemon.name  
+    )
+end
+
+20.times do
+    Power.create!(
+
+        name:Faker::Superhero.power,
+        description:Faker::Lorem.paragraph
+    )
+end
+Hero.all.each do |hero|
+    Power.all.sample(12).each do |power|
+      HeroPower.create!(
+        hero_id: hero.id,
+        power_id: power.id,
+        strength: Faker::Games::Overwatch.hero 
+      )
+    end
+  end
+puts "✅ Done seeding!"
